7fa5cc7778985f56268678360bcc94c9e5e9d32b
woon <dldnstkfkd@gmail.com>
2023-10-13 19:22:14 +0900

    update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,6 +1,9 @@
 package com.basic.myspringboot.runner;
 
 
+import com.basic.myspringboot.dto.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -10,7 +13,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-@Order(1) // () 숫자가 낮을수록 우선순위 높음
+@Order(2) // () 숫자가 낮을수록 우선순위 높음    // DatabaseRunner 우선선위가 더 높음
 public class MyRunner implements ApplicationRunner {
 
     @Value("${myboot.name}")
@@ -25,15 +28,28 @@ public class MyRunner implements ApplicationRunner {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private Customer customer;
+
+
+    Logger logger = LoggerFactory.getLogger(MyRunner.class);
+
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Port Number = " + environment.getProperty("local.server.port"));
-        System.out.println("myboot.name = " + name);
-        System.out.println("myboot.age = " + age);
-        System.out.println("myboot.fullName = " + fullName);
-        System.out.println(">>> Git Test");
+        //info
+        logger.info("Logger 클래스이름 {}", logger.getClass().getName());
+        logger.info("Customer 현재모드= {}", customer.getName() );//,뒤의 값이 {}안에 들어온다.
+        logger.info("Port Number = {}", environment.getProperty("local.server.port"));
+        logger.info("myboot.name = {}", name);
+        logger.info("myboot.age = {}", age);
+        logger.info("myboot.fullName = {}", fullName);
+        logger.info(">>> Git Test");
 
-        System.out.println("VM Argument foo = " + args.containsOption("foo"));
-        System.out.println("Program Arguement bar = " + args.containsOption("bar"));
+        //debug
+        logger.debug("VM Argument foo = {}, Program Arguement bar = {}",
+                args.containsOption("foo"),
+                args.containsOption("bar")
+                );
     }
 }
